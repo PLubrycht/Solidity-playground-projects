6ab4833e2c5c1d7d27b13af62cf9d7df61e55f37
PLubrycht <plubrycht@gmail.com>
2025-10-27 23:35:12 +0100

    NodeJS API

--- dapp/token-api/src/app.controller.ts
+++ dapp/token-api/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,30 @@ export class AppController {
   @Get('token-name')
   async getTokenName() {
     return { result: await this.appService.getTokenName() };
+  }
+
+  @Get('total-supply')
+  async getTotalSupply() {
+    return { result: await this.appService.getTotalSupply() };
+  }
+
+  @Get('token-balance/:address')
+  async getTokenBalance(@Param('address') address: string) {
+    return { result: await this.appService.getTokenBalance(address) };
+  }
+
+  @Get('transaction-receipt')
+  async getTransactionReceipt(@Query('hash') hash: string) {
+    return { result: await this.appService.getTransactionReceipt(hash) };
+  }
+
+  @Get('server-wallet-address')
+  getServerWalletAddress() {
+    return { result: this.appService.getServerWalletAddress() };
+  }
+
+  @Get('check-minter-role')
+  async checkMinterRole(@Query('address') address: string) {
+    return { result: await this.appService.checkMinterRole(address) };
   }
 }
--- dapp/token-api/src/app.service.ts
+++ dapp/token-api/src/app.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import * as tokenJson from './assets/MyToken.json';
-import { Address, createPublicClient, http } from 'viem';
+import {
+  Address,
+  createPublicClient,
+  formatEther,
+  http,
+  type PublicClient,
+} from 'viem';
 import { sepolia } from 'viem/chains';
+import { createWalletClient } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
 
 import * as dotenv from 'dotenv';
 
@@ -17,6 +25,21 @@ console.log('providerApiKey', providerApiKey.slice(0, 5), '...');
 
 @Injectable()
 export class AppService {
+  private readonly publicClient: PublicClient;
+  private readonly walletClient: ReturnType<typeof createWalletClient>;
+
+  constructor() {
+    const account = privateKeyToAccount(`0x${process.env.PRIVATE_KEY}`);
+    this.publicClient = createPublicClient({
+      chain: sepolia,
+      transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+    });
+    this.walletClient = createWalletClient({
+      transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+      chain: sepolia,
+      account: account,
+    });
+  }
   getHello(): string {
     return 'Hello World!';
   }
@@ -24,17 +47,58 @@ export class AppService {
     return tokenAddress;
   }
   async getTokenName(): Promise<string> {
-    const publicClient = createPublicClient({
-      chain: sepolia,
-      transport: http(
-        `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      ),
-    });
-    const name = await publicClient.readContract({
+    const name = await this.publicClient.readContract({
       address: this.getContractAddress() as Address,
       abi: tokenJson.abi,
       functionName: 'name',
     });
     return name as string;
+  }
+  async getTotalSupply() {
+    const totalSupplyBN = await this.publicClient.readContract({
+      address: this.getContractAddress() as Address,
+      abi: tokenJson.abi,
+      functionName: 'totalSupply',
+    });
+    const totalSupply = formatEther(totalSupplyBN as bigint);
+    return totalSupply;
+  }
+  async getTokenBalance(address: string) {
+    const balance = await this.publicClient.readContract({
+      address: this.getContractAddress() as Address,
+      abi: tokenJson.abi,
+      functionName: 'balanceOf',
+      args: [address as Address],
+    });
+    const balanceFormatted = formatEther(balance as bigint);
+    return balanceFormatted;
+  }
+  async getTransactionReceipt(hash: string) {
+    await Promise.resolve();
+    const receipt = await this.publicClient.getTransactionReceipt({
+      hash: hash as Address,
+    });
+    return receipt;
+  }
+
+  getServerWalletAddress(): string {
+    return this.walletClient.account?.address ?? '';
+  }
+
+  async checkMinterRole(address: string): Promise<boolean> {
+    const MINTER_ROLE =
+      '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6';
+    // const MINTER_ROLE =  await this.publicClient.readContract({
+    //   address: this.getContractAddress(),
+    //   abi: tokenJson.abi,
+    //   functionName: 'MINTER_ROLE'
+    // });
+    const hasRole = await this.publicClient.readContract({
+      address: this.getContractAddress() as Address,
+      abi: tokenJson.abi,
+      functionName: 'hasRole',
+      args: [MINTER_ROLE, address],
+    });
+    return hasRole as boolean;
   }
 }
